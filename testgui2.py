846967dcaa961e18a5d7a65d7016b01fd299ed46
ConnorSp0 <122158578+ConnorSp0@users.noreply.github.com>
2023-02-25 21:23:50 +0800

    Finished Receipt Algo
    Insertion of items, quantities, and price of those certain items. Insertion of records in receipt table. Deletion of records in receipt table.

--- testgui2.py
+++ testgui2.py
@@ -5,8 +5,13 @@
 
 from pathlib import Path
 from os.path import dirname
-# Explicit imports to satisfy Flake8
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage,Radiobutton
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, Radiobutton
+from tkinter import *
+from tkinter import ttk
+from formdb import Database
+import tkinter.messagebox as messageBox
+
+db = Database("mydatabase.db")
 
 
 OUTPUT_PATH = dirname(__file__)
@@ -16,24 +21,25 @@ ASSETS_PATH = OUTPUT_PATH + r"\assets\frame0"
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
+
 print(OUTPUT_PATH)
 window = Tk()
-
+window.title("Store Receipt Database")
 window.geometry("1055x731")
-window.configure(bg = "#074A5E")
+window.configure(bg="#074A5E")
 
 
 canvas = Canvas(
     window,
-    bg = "#074A5E",
-    height = 731,
-    width = 1055,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
+    bg="#074A5E",
+    height=731,
+    width=1055,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge"
 )
 
-canvas.place(x = 0, y = 0)
+canvas.place(x=0, y=0)
 canvas.create_rectangle(
     35.0,
     35.0,
@@ -43,177 +49,272 @@ canvas.create_rectangle(
     outline="")
 
 
-itemClicked = ""
+itemClicked = StringVar(value="0")
 
-RButton_vinegar = Radiobutton(window, text = "Vinegar",  variable= itemClicked , value="vinegar",background="#FFFFFF")
+# program functions
+
+priceList = [8, 50, 10, 45, 10, 8, 8, 8, 5, 5, 18, 8,
+             7, 8, 260, 15, 10, 50, 10, 8, 5, 150, 8, 10, 60]
+itemList = ["vinegar", "sugar", "soy", "salt", "oil", "egg", "potato", "tomato", "onion", "garlic", "sardines", "biscuit",
+            "coffee", "milo", "milk", "ariel", "downy", "bars", "joy", "toothpaste", "mask", "alcohol", "shampoo", "conditioner", "rice"]
+
+
+def dispalyAll():
+    tv2.delete(*tv2.get_children())
+    for row in db.fetch():
+        tv2.insert("", END, values=row)
+        # print(row)
+
+
+def submitSelection():
+    item = itemClicked.get()
+    price = 0.0
+    if item in itemList:
+        i = itemList.index(item)
+    price = priceList[i]
+    quantity = entry_1.get()
+    try:
+        int(quantity)
+    except:
+        messageBox.showinfo("Item Insertion Status", "Input whole numbers in 'Quantity'")
+        checkError = 1
+    else:
+        price = price * float(quantity)
+        price = "%.2f" % price
+        tv.insert("","end",values=(item, quantity, price))
+
+def submitReceipt():
+    checkError =0
+    totalfee = 0.0
+    for child in tv.get_children():
+        totalfee += float(tv.item(child)["values"][2])
+    payment = entry_4.get()
+    if totalfee == 0 : 
+        messageBox.showinfo("Receipt Submission Status", "Items need to be ordered first")
+        checkError = 1
+    elif payment == "" : 
+        messageBox.showinfo("Receipt Submission Status", "No Payment inputted")
+        checkError = 1
+    else:
+        try:
+            int(payment)
+        except:
+            messageBox.showinfo("Receipt Submission Status", "Payment inputted is not a number")
+            checkError = 1
+        else:
+            if float(payment) < totalfee:
+                messageBox.showinfo("Receipt Submission Status", "Total Fee higher than Payment")
+                checkError = 1
+            if checkError == 0:
+                change = float(payment) - totalfee
+                change = "%.2f" % change
+                totalfee = "%.2f" % totalfee
+
+                entry_2.config(state = NORMAL)   #Total Fee Display
+                entry_2.insert(0, str(totalfee))
+                entry_2.config(state = DISABLED)
+
+                entry_5.config(state = NORMAL) 
+                entry_5.insert(0, str(change)) #Total Change Display
+                entry_5.config(state = DISABLED)
+
+def deleteReceipt():
+    tv.delete(*tv.get_children())
+
+RButton_vinegar = Radiobutton(
+    window, text="Vinegar",  variable=itemClicked, value="vinegar", background="#FFFFFF")
 RButton_vinegar.place(
     x=60.0,
     y=100.0,
     width=100.0,
     height=30.0)
 
-RButton_sugar = Radiobutton(window, text = "Sugar",  variable= itemClicked , value="sugar",background="#FFFFFF")
+RButton_sugar = Radiobutton(
+    window, text="Sugar",  variable=itemClicked, value="sugar", background="#FFFFFF")
 RButton_sugar.place(
     x=57.0,
     y=135.0,
     width=100.0,
     height=30.0)
 
-RButton_soy = Radiobutton(window, text = "Soy Sauce",  variable= itemClicked , value="soy",background="#FFFFFF")
+RButton_soy = Radiobutton(window, text="Soy Sauce",
+                          variable=itemClicked, value="soy", background="#FFFFFF")
 RButton_soy.place(
     x=57.0,
     y=170.0,
     width=100.0,
     height=30.0)
 
-RButton_salt = Radiobutton(window, text = "Salt",  variable= itemClicked , value="salt",background="#FFFFFF")
+RButton_salt = Radiobutton(
+    window, text="Salt",  variable=itemClicked, value="salt", background="#FFFFFF")
 RButton_salt.place(
     x=57.0,
     y=205.0,
     width=100.0,
     height=30.0)
 
-RButton_oil = Radiobutton(window, text = "Oil",  variable = itemClicked , value="oil",background="#FFFFFF")
+RButton_oil = Radiobutton(
+    window, text="Oil",  variable=itemClicked, value="oil", background="#FFFFFF")
 RButton_oil.place(
     x=57.0,
     y=240.0,
     width=100.0,
     height=30.0)
 
-RButton_egg = Radiobutton(window, text = "Egg",  variable = itemClicked , value="egg",background="#FFFFFF")
+RButton_egg = Radiobutton(
+    window, text="Egg",  variable=itemClicked, value="egg", background="#FFFFFF")
 RButton_egg.place(
     x=165.0,
     y=100.0,
     width=100.0,
     height=30.0)
 
-RButton_potato = Radiobutton(window, text = "Potato",  variable = itemClicked , value="potato",background="#FFFFFF")
+RButton_potato = Radiobutton(
+    window, text="Potato",  variable=itemClicked, value="potato", background="#FFFFFF")
 RButton_potato.place(
     x=165.0,
     y=135.0,
     width=100.0,
     height=30.0)
 
-RButton_tomato = Radiobutton(window, text = "Tomato",  variable = itemClicked , value="tomato",background="#FFFFFF")
+RButton_tomato = Radiobutton(
+    window, text="Tomato",  variable=itemClicked, value="tomato", background="#FFFFFF")
 RButton_tomato.place(
     x=165.0,
     y=170.0,
     width=100.0,
     height=30.0)
 
-RButton_onion = Radiobutton(window, text = "Onion",  variable = itemClicked , value="onion",background="#FFFFFF")
+RButton_onion = Radiobutton(
+    window, text="Onion",  variable=itemClicked, value="onion", background="#FFFFFF")
 RButton_onion.place(
     x=165.0,
     y=205.0,
     width=100.0,
     height=30.0)
 
-RButton_garlic = Radiobutton(window, text = "Garlic",  variable = itemClicked , value="garlic",background="#FFFFFF")
+RButton_garlic = Radiobutton(
+    window, text="Garlic",  variable=itemClicked, value="garlic", background="#FFFFFF")
 RButton_garlic.place(
     x=165.0,
     y=240.0,
     width=100.0,
     height=30.0)
 
-RButton_sardines = Radiobutton(window, text = "Sardines",  variable = itemClicked , value="sardines",background="#FFFFFF")
+RButton_sardines = Radiobutton(
+    window, text="Sardines",  variable=itemClicked, value="sardines", background="#FFFFFF")
 RButton_sardines.place(
     x=270.0,
     y=100.0,
     width=100.0,
     height=30.0)
 
-RButton_biscuit = Radiobutton(window, text = "Biscuit",  variable = itemClicked , value="biscuit",background="#FFFFFF")
+RButton_biscuit = Radiobutton(
+    window, text="Biscuit",  variable=itemClicked, value="biscuit", background="#FFFFFF")
 RButton_biscuit.place(
     x=270.0,
     y=135.0,
     width=100.0,
     height=30.0)
 
-RButton_coffee = Radiobutton(window, text = "Instant Coffee",  variable = itemClicked , value="coffee",background="#FFFFFF")
+RButton_coffee = Radiobutton(window, text="Instant Coffee",
+                             variable=itemClicked, value="coffee", background="#FFFFFF")
 RButton_coffee.place(
     x=270.0,
     y=170.0,
     width=100.0,
     height=30.0)
 
-RButton_milo = Radiobutton(window, text = "Milo",  variable = itemClicked , value="milo",background="#FFFFFF")
+RButton_milo = Radiobutton(
+    window, text="Milo",  variable=itemClicked, value="milo", background="#FFFFFF")
 RButton_milo.place(
     x=270.0,
     y=205.0,
     width=100.0,
     height=30.0)
 
-RButton_milk = Radiobutton(window, text = "Milk",  variable = itemClicked , value="milk",background="#FFFFFF")
+RButton_milk = Radiobutton(
+    window, text="Milk",  variable=itemClicked, value="milk", background="#FFFFFF")
 RButton_milk.place(
     x=270.0,
     y=240.0,
     width=100.0,
     height=30.0)
 
-RButton_ariel = Radiobutton(window, text = "Ariel",  variable = itemClicked , value="ariel",background="#FFFFFF")
+RButton_ariel = Radiobutton(
+    window, text="Ariel",  variable=itemClicked, value="ariel", background="#FFFFFF")
 RButton_ariel.place(
     x=375.0,
     y=100.0,
     width=100.0,
     height=30.0)
 
-RButton_downy = Radiobutton(window, text = "Downy",  variable = itemClicked , value="downy",background="#FFFFFF")
+RButton_downy = Radiobutton(
+    window, text="Downy",  variable=itemClicked, value="downy", background="#FFFFFF")
 RButton_downy.place(
     x=375.0,
     y=135.0,
     width=100.0,
     height=30.0)
 
-RButton_bars = Radiobutton(window, text = "Bar Soap",  variable = itemClicked , value="bars",background="#FFFFFF")
+RButton_bars = Radiobutton(window, text="Bar Soap",
+                           variable=itemClicked, value="bars", background="#FFFFFF")
 RButton_bars.place(
     x=375.0,
     y=170.0,
     width=100.0,
     height=30.0)
 
-RButton_joy = Radiobutton(window, text = "Joy",  variable = itemClicked , value="joy",background="#FFFFFF")
+RButton_joy = Radiobutton(
+    window, text="Joy",  variable=itemClicked, value="joy", background="#FFFFFF")
 RButton_joy.place(
     x=375.0,
     y=205.0,
     width=100.0,
     height=30.0)
 
-RButton_toothpaste = Radiobutton(window, text = "Toothpaste",  variable = itemClicked , value="toothpaste",background="#FFFFFF")
+RButton_toothpaste = Radiobutton(
+    window, text="Toothpaste",  variable=itemClicked, value="toothpaste", background="#FFFFFF")
 RButton_toothpaste.place(
     x=375.0,
     y=240.0,
     width=100.0,
     height=30.0)
 
-RButton_mask = Radiobutton(window, text = "Mask",  variable = itemClicked , value="mask",background="#FFFFFF")
+RButton_mask = Radiobutton(
+    window, text="Mask",  variable=itemClicked, value="mask", background="#FFFFFF")
 RButton_mask.place(
     x=480.0,
     y=100.0,
     width=100.0,
     height=30.0)
 
-RButton_alcohol = Radiobutton(window, text = "Alcohol",  variable = itemClicked , value="alcohol",background="#FFFFFF")
+RButton_alcohol = Radiobutton(
+    window, text="Alcohol",  variable=itemClicked, value="alcohol", background="#FFFFFF")
 RButton_alcohol.place(
     x=480.0,
     y=135.0,
     width=100.0,
     height=30.0)
 
-RButton_shampoo = Radiobutton(window, text = "Shampoo",  variable = itemClicked , value="shampoo",background="#FFFFFF")
+RButton_shampoo = Radiobutton(
+    window, text="Shampoo",  variable=itemClicked, value="shampoo", background="#FFFFFF")
 RButton_shampoo.place(
     x=480.0,
     y=170.0,
     width=100.0,
     height=30.0)
 
-RButton_conditioner = Radiobutton(window, text = "Conditioner",  variable = itemClicked , value="conditioner",background="#FFFFFF")
+RButton_conditioner = Radiobutton(
+    window, text="Conditioner",  variable=itemClicked, value="conditioner", background="#FFFFFF")
 RButton_conditioner.place(
     x=480.0,
     y=205.0,
     width=100.0,
     height=30.0)
 
-RButton_rice = Radiobutton(window, text = "Rice Grain",  variable = itemClicked , value="rice",background="#FFFFFF")
+RButton_rice = Radiobutton(window, text="1 kilo Rice Grain",
+                           variable=itemClicked, value="rice", background="#FFFFFF")
 RButton_rice.place(
     x=480.0,
     y=240.0,
@@ -226,7 +327,7 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_1 clicked"),
+    command=submitSelection,
     relief="flat"
 )
 button_1.place(
@@ -464,42 +565,23 @@ canvas.create_rectangle(
     outline="")
 
 entry_image_2 = PhotoImage(
-    file=relative_to_assets("entry_2.png"))
+    file=relative_to_assets("entry_4.png"))
 entry_bg_2 = canvas.create_image(
-    857.5,
+    905.5,
     446.5,
     image=entry_image_2
 )
 entry_2 = Entry(
+    state=DISABLED,
     bd=0,
     bg="#D9D9D9",
     fg="#000716",
     highlightthickness=0
 )
 entry_2.place(
-    x=823.0,
-    y=432.0,
-    width=69.0,
-    height=27.0
-)
-
-entry_image_3 = PhotoImage(
-    file=relative_to_assets("entry_3.png"))
-entry_bg_3 = canvas.create_image(
-    945.0,
-    446.5,
-    image=entry_image_3
-)
-entry_3 = Entry(
-    bd=0,
-    bg="#D9D9D9",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_3.place(
-    x=899.0,
-    y=432.0,
-    width=92.0,
+    x=822.0,
+    y=434.0,
+    width=168.0,
     height=27.0
 )
 
@@ -531,6 +613,7 @@ entry_bg_5 = canvas.create_image(
     image=entry_image_5
 )
 entry_5 = Entry(
+    state=DISABLED,
     bd=0,
     bg="#D9D9D9",
     fg="#000716",
@@ -625,7 +708,7 @@ button_4 = Button(
     image=button_image_4,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_4 clicked"),
+    command=deleteReceipt,
     relief="flat"
 )
 button_4.place(
@@ -641,7 +724,7 @@ button_5 = Button(
     image=button_image_5,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_5 clicked"),
+    command=submitReceipt,
     relief="flat"
 )
 button_5.place(
@@ -677,5 +760,38 @@ canvas.create_text(
     fill="#000000",
     font=("TimesNewRomanPSMT", 18 * -1)
 )
-window.resizable(False, False)
+
+tv = ttk.Treeview(columns=(1, 2, 3), style="mystyle.Treeview")
+tv.place(
+    x=675.0,
+    y=165.0,
+    width=320,
+    height=225)
+tv.heading("1", text="ITEM")
+tv.column("1", width=50)
+tv.heading("2", text="QTY")
+tv.column("2", width=50)
+tv.heading("3", text="SUBTOTAL")
+tv.column("3", width=50)
+tv['show'] = 'headings'
+
+
+tv2 = ttk.Treeview(columns=(1, 2, 3, 4), style="mystyle.Treeview")
+tv2.place(
+    x=35.0,
+    y=440.0,
+    width=596,
+    height=256)
+tv2.heading("1", text="Transaction ID")
+tv2.column("1", width=50)
+tv2.heading("2", text="Total Fee")
+tv2.column("2", width=50)
+tv2.heading("3", text="Payment")
+tv2.column("3", width=50)
+tv2.heading("4", text="Change")
+tv2.column("4", width=50)
+tv2['show'] = 'headings'
+
+
+dispalyAll()
 window.mainloop()
